d530f67097a287ef31851b0424e6c0c4fe2c240e
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-09-03 14:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 class Animal {
-            constructor(species) {
-                this._species = species;
-            }
+    constructor(species) {
+        this._species = species;
+    }
 
-            get species() {
-                return this._species;
-            }
+    get species() {
+        return this._species;
+    }
 
-            set species(value) {
-                this._species = value;
-            }
+    set species(value) {
+        this._species = value;
+    }
 
-            makeSound() {
-                console.log(`The ${this._species} makes a sound.`);
-            }
-        }
+    makeSound() {
+        console.log(`The ${this._species} makes a sound`);
+    }
+}
 
-        class Dog extends Animal {
-            bark() {
-                console.log("woof");
+class Dog extends Animal {
+    bark() {
+        console.log("woof");
+    }
+}
 
-            }
-        }
-
-       class Cat extends Animal {
-            purr() {
-                console.log("purr");
-            }
-        }
+class Cat extends Animal {
+    purr() {
+        console.log("purr");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
